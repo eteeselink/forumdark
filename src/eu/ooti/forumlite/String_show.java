d9232c81318702d44a66a1af63aee8ef574cc855
unknown <PKotsis@nbwin1164.campus.tue.nl>
2015-10-07 16:54:25 +0200

    made method static

--- src/eu/ooti/forumlite/String_show.java
+++ src/eu/ooti/forumlite/String_show.java
@@ -1,9 +1,23 @@
 package eu.ooti.forumlite;
 
+import static java.lang.System.out;
+
 public class String_show {
-	public boolean[][] string_show  (String str)
+	public static boolean[][] string_show  (String str)
 	{
 		boolean result[][] = new boolean[5][5];
+		
+		for( int i = 0; i<5; ++i)
+		{
+			for(int j=0; j<5; ++j)
+			{
+				out.println(result[i][j]);
+			}
+		}
+		
+		/*if (str == "o")
+			result = */
+		
 		return result;
 	}
 }
