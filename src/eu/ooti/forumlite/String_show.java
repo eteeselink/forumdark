f4e01bf877e8abfc6d904cb9e0d19d5dc698355d
unknown <PKotsis@nbwin1164.campus.tue.nl>
2015-10-07 17:13:32 +0200

    update string_show

--- src/eu/ooti/forumlite/String_show.java
+++ src/eu/ooti/forumlite/String_show.java
@@ -2,22 +2,33 @@ package eu.ooti.forumlite;
 
 import static java.lang.System.out;
 
-public class String_show {
-	public static boolean[][] string_show  (String str)
+import com.googlecode.lanterna.screen.Screen;
+
+public class String_show implements AutoCloseable {
+	public static int[][] string_show  (String str)
 	{
-		boolean result[][] = new boolean[5][5];
+		int result[][] = new int[5][5];
+		
+		int testResult[][] = {{0,0,0,0,0},{0,1,1,1,0},{0,1,0,1,0},{0,1,1,1,0},{0,0,0,0,0}};
+		
+		result = testResult;
 		
 		for( int i = 0; i<5; ++i)
 		{
 			for(int j=0; j<5; ++j)
 			{
-				out.println(result[i][j]);
+				out.print(result[i][j] + " ");
 			}
+			out.println();
 		}
 		
 		/*if (str == "o")
 			result = */
 		
 		return result;
+	}
+	
+	public void close() throws Exception {
+		screen.stopScreen();
 	}
 }
