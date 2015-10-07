7a26abb010c0075fbde3d0182b9dfd722c685498
unknown <IAnastas@nbwin1160.campus.tue.nl>
2015-10-07 16:59:32 +0200

    added new fiels

---
+++ src/eu/ooti/forumlite/MovieReader.java
@@ -0,0 +1,25 @@
+package eu.ooti.forumlite;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MovieReader {
+	
+	public static void ReadMovie()
+	{
+		 BufferedReader br = null;
+	        String strLine = "";
+	        try {
+	            br = new BufferedReader( new FileReader("files\\Movie1.txt"));
+	            while( (strLine = br.readLine()) != null){
+	                System.out.println(strLine);
+	                String_show.string_show(strLine);
+	                // sleep
+	                Thread.sleep(2000);
+	            }
+	        }
+		catch(Exception e){}
+	}
+
+}
