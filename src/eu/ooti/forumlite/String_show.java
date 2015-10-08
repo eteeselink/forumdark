c6e98b853462c27ade8fe1cc6fd3bc2fe4893e81
unknown <PKotsis@nbwin1164.campus.tue.nl>
2015-10-08 13:19:29 +0200

    no message

--- src/eu/ooti/forumlite/String_show.java
+++ src/eu/ooti/forumlite/String_show.java
@@ -5,11 +5,6 @@ import java.util.List;
 
 public class String_show {
 	public static List<int[][]> makeCharacters(String str) throws Exception {
-		/*int matrix0[][] = {{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
-		int matrix1[][] = {{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
-		int matrix2[][] = {{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
-		int matrix3[][] = {{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
-		int matrix4[][] = {{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};*/
 		
 		ArrayList<int[][]> matrices = new ArrayList<int[][]>();
 		
@@ -88,7 +83,6 @@ public class String_show {
 	public static void string_show (String str, String colour, Display display) throws Exception
 	{
 		List<int[][]> matrices = makeCharacters(str);
-		display.display(matrices, colour);	
-		//display.display(matrices.get(0),matrices.get(1), matrices.get(2), matrices.get(3), matrices.get(4), colour);	
+		display.display(matrices, colour);		
 	}
 }
