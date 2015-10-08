e93ad613908328bdb51e171a2b5455aeecd7ca09
IMikovsk <IMikovsk@nbwin1167.campus.tue.nl>
2015-10-08 11:02:53 +0200

    Fixed Display

--- src/eu/ooti/forumlite/CLI.java
+++ src/eu/ooti/forumlite/CLI.java
@@ -13,12 +13,8 @@ import java.util.StringTokenizer;
  *
  */
 public class CLI {
-<<<<<<< HEAD
 	
 	static String_show show = null;
-	Display display = new Display();
-=======
->>>>>>> 9aa04e114cb08dabedc0ab2b2be1d16654a7fd78
 
 	/**
 	 * @param args
@@ -26,7 +22,7 @@ public class CLI {
 	 */
 	public void lunchCLI() throws InterruptedException {
 		boolean exit = false;
-		
+		Display display = new Display();
 		printHelp();
 		
 		while(!exit){
@@ -36,66 +32,33 @@ public class CLI {
 			System.out.println("Input >> " + strInput);
 			//multiple tokens on line
 			List<String> arguments = new ArrayList<>();
-<<<<<<< HEAD
-			if(!strInput.isEmpty()){
-				arguments = extractArgs(strInput);
-				String command = arguments.get(0);
-				if(command.equalsIgnoreCase("show")){
-	//				String_show show = new String_show();
-	//				show.string_show(arguments.get(1));	
-	//				if(show == null){
-	//					show = new String_show();
-	//				}
-	//				show.string_show(arguments.get(1));
-					try {
-						String_show.string_show(arguments.get(1), display);
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				
-				if (command.equalsIgnoreCase("movie")){
-					MovieReader.ReadMovie(display);
-				}
-				if(command.equalsIgnoreCase("exit") || command.equalsIgnoreCase("q")){
-					exit = true;
-					System.out.println("Bye bye!");
-=======
 			arguments = extractArgs(strInput);
 			
 			String command = arguments.get(0);
 			if(command.equalsIgnoreCase("show")){
 //				String_show show = new String_show();
 //				show.string_show(arguments.get(1));	
+//				if(show == null){
+//					show = new String_show();
+//				}
+//				show.string_show(arguments.get(1));
 				try {
-					String_show.string_show(arguments.get(1));
+					String_show.string_show(arguments.get(1),display);
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
->>>>>>> 9aa04e114cb08dabedc0ab2b2be1d16654a7fd78
 				}
 			}
 			
 			if (command.equalsIgnoreCase("movie")){
-				MovieReader.ReadMovie();
+				MovieReader.ReadMovie(display);
 			}
-//			if (command.equalsIgnoreCase("moviefile")){
-//				System.out.print("Input directory: ");
-//				Scanner scannerMovie = new Scanner(System.in);
-//				String strMovie = scannerMovie.nextLine();
-//				System.out.println("Input >> " + strInput);
-//				MovieReader.ReadMovieParameter(".\\files\\Movie1.txt");
-//			}
 			if(command.equalsIgnoreCase("exit") || command.equalsIgnoreCase("q")){
 				exit = true;
 				System.out.println("Bye bye!");
 			}
 		}
 		// TODO need to show help
-		
-		
-				
 	}
 
 	private static void printHelp() {
@@ -103,7 +66,7 @@ public class CLI {
 		System.out.println("show <word>  -- shows the word on the display");
 		System.out.println("movie <moviename> -- reads file with name=moviname");
 //		System.out.println("color 0..255 0..255 0..255 [0..5] -- sets the RGB color of the number of lights");
-		System.out.println("interval <number>  -- sets the time between frames of the movie");  
+//		System.out.println("interval <number>  -- sets the time between frames of the movie");  
 //		System.out.println("USAGE");
 		System.out.println("exit / q  -- exit from the application");
 	}
