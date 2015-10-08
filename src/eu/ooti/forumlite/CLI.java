8d2107c64de2ccfc8b277b28071e66100c1fd9f6
unknown <FButnari@nbwin1166.campus.tue.nl>
2015-10-08 12:22:11 +0200

    all ok

--- src/eu/ooti/forumlite/CLI.java
+++ src/eu/ooti/forumlite/CLI.java
@@ -51,7 +51,7 @@ public class CLI {
 			}
 			
 			if (command.equalsIgnoreCase("movie")){
-				MovieReader.ReadMovie(display, "red");
+				MovieReader.ReadMovie(display, arguments.get(1));
 			}
 			if(command.equalsIgnoreCase("exit") || command.equalsIgnoreCase("q")){
 				exit = true;
